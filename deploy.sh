1964d4ec5a278be8a951d99bf20961acafc58843
skysamer <smtkdals94@gmail.com>
2023-11-05 18:06:57 +0900

    feature : 배포스크립트 수정
    - 도커 이미지 빌드 시 캐시 미사용으로 수정"
    - 컨테이너 동작 안할 시 예외처리 추가

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
-docker build -t salary .
+docker build --no-cache -t was .
 
-docker stop was
+CONTAINER_NAME="was"
 
-docker rm was
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+fi
 
 docker run --env-file /root/.env -d -p 8080:8080 --name was was
