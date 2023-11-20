f1c225f4596c5927d966d5ba78d9d577daa5b252
skysamer <smtkdals94@gmail.com>
2023-11-20 22:44:37 +0900

    feature : 빌드 스크립트 수정
    - 컨테이너 중복 시 예외처리 개선

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,16 @@ docker build --no-cache -t was .
 
 CONTAINER_NAME="was"
 
+# 컨테이너가 실행 중이면 정지 후 삭제
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+  echo "Container $CONTAINER_NAME is running. Stopping and removing..."
   docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+fi
+
+# 컨테이너가 이미 존재하면 정지 후 삭제
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+  echo "Container $CONTAINER_NAME exists. Removing..."
   docker rm $CONTAINER_NAME
 fi
 
