a95dce5df5bef36a14bf8048d5c40a87282d47a1
skysamer <smtkdals94@gmail.com>
2024-03-04 23:34:59 +0900

    feat : 무중단배포 구성
    - 컨테이너 이름도 동적으로 변경되도록 수정

--- deploy_main.sh
+++ deploy_main.sh
@@ -27,13 +27,18 @@ fi
 
 echo "Container is running on port $current_port"
 
+new_container_name=""
+old_container_name=""
+
 # 새로운 포트 계산
 if [ "$current_port" == "8080" ]; then
   new_port=9090
   new_container_name=was_main_2
+  old_container_name=was_main_1
 else
   new_port=8080
   new_container_name=was_main_1
+  old_container_name=was_main_2
 fi
 
 echo "Starting new container $new_container_name on port $new_port"
@@ -51,7 +56,7 @@ docker run --env-file /root/.env -d -p $new_port:$new_port -e SPRING_PROFILES_AC
 echo "Updated Nginx proxy_pass to port $new_port"
 
 # 기존 컨테이너 중지 및 삭제 (동적으로 변경)
-docker stop $new_container_name
-docker rm $new_container_name
+docker stop $old_container_name
+docker rm $old_container_name
 
 echo "Stopped and removed old container running on port $current_port"
