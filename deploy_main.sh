df8fdfc6bc4c5b5823fc369cb4dfdb440759b8ee
skysamer <smtkdals94@gmail.com>
2024-03-04 22:08:51 +0900

    feat : 무중단배포 구성
    - 1차 원복처리

--- deploy_main.sh
+++ deploy_main.sh
@@ -1,58 +1,56 @@
-#docker build --no-cache -t was_main .
-#
-#CONTAINER_NAME="was_main"
-#
-## 컨테이너가 실행 중이면 정지 후 삭제
-#if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-#  echo "Container $CONTAINER_NAME is running. Stopping and removing..."
-#  docker stop $CONTAINER_NAME
-#  docker rm $CONTAINER_NAME
-#fi
-#
-## 컨테이너가 이미 존재하면 정지 후 삭제
-#if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-#  echo "Container $CONTAINER_NAME exists. Removing..."
-#  docker rm $CONTAINER_NAME
-#fi
-#
-#docker run --env-file /root/.env -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=proc -v /applog/main:/logs --name was_main was_main
+docker build --no-cache -t was_main .
 
+CONTAINER_NAME="was_main"
 
-#!/bin/bash
-
-# 현재 실행 중인 컨테이너의 포트 확인
-current_port=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' was_main 2>/dev/null)
-
-if [ -z "$current_port" ]; then
-  current_port=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "9090/tcp") 0).HostPort}}' was_main 2>/dev/null)
+# 컨테이너가 실행 중이면 정지 후 삭제
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+  echo "Container $CONTAINER_NAME is running. Stopping and removing..."
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
 fi
 
-if [ -z "$current_port" ]; then
-  echo "No container is running on port 8080 or 9090"
-else
-  echo "Container is running on port $current_port"
-
-  # 새로운 포트 계산
-  if [ "$current_port" == "8080" ]; then
-    new_port=9090
-  else
-    new_port=8080
-  fi
-
-  echo "Starting new container on port $new_port"
-
-  # 새로운 컨테이너 시작
-  docker run --env-file /root/.env -d -p $new_port:$new_port -e SPRING_PROFILES_ACTIVE=proc -v /applog/main:/logs --name was_main_new was_main
-
-  # 엔진엑스 프록시 패스 업데이트
-  sed -i "s|proxy_pass .*;|proxy_pass http://127.0.0.1:$new_port;|" /etc/nginx/nginx.conf
-  nginx -s reload
-
-  echo "Updated Nginx proxy_pass to port $new_port"
-
-  # 기존 컨테이너 중지 및 삭제
-  docker stop was_main
-  docker rm was_main
-
-  echo "Stopped and removed old container running on port $current_port"
+# 컨테이너가 이미 존재하면 정지 후 삭제
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+  echo "Container $CONTAINER_NAME exists. Removing..."
+  docker rm $CONTAINER_NAME
 fi
+
+docker run --env-file /root/.env -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=proc -v /applog/main:/logs --name was_main was_main
+
+
+## 현재 실행 중인 컨테이너의 포트 확인
+#current_port=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' was_main 2>/dev/null)
+#
+#if [ -z "$current_port" ]; then
+#  current_port=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "9090/tcp") 0).HostPort}}' was_main 2>/dev/null)
+#fi
+#
+#if [ -z "$current_port" ]; then
+#  echo "No container is running on port 8080 or 9090"
+#else
+#  echo "Container is running on port $current_port"
+#
+#  # 새로운 포트 계산
+#  if [ "$current_port" == "8080" ]; then
+#    new_port=9090
+#  else
+#    new_port=8080
+#  fi
+#
+#  echo "Starting new container on port $new_port"
+#
+#  # 새로운 컨테이너 시작
+#  docker run --env-file /root/.env -d -p $new_port:$new_port -e SPRING_PROFILES_ACTIVE=proc -v /applog/main:/logs --name was_main_new was_main
+#
+#  # 엔진엑스 프록시 패스 업데이트
+#  sed -i "s|proxy_pass .*;|proxy_pass http://127.0.0.1:$new_port;|" /etc/nginx/nginx.conf
+#  nginx -s reload
+#
+#  echo "Updated Nginx proxy_pass to port $new_port"
+#
+#  # 기존 컨테이너 중지 및 삭제
+#  docker stop was_main
+#  docker rm was_main
+#
+#  echo "Stopped and removed old container running on port $current_port"
+#fi
